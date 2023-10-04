8d12333cccd264dd715ac9ff980b50a6b1433d59
Nicolas <chastinlive.fr@gmail.com>
2023-10-04 15:12:14 +0200

    [add] Button Sprite Component Button Sprite is used to create interaction with a sprite when hovered or clicked through a lambda

---
+++ src/Components/ButtonSprite/ButtonSprite.cpp
@@ -0,0 +1,160 @@
+/**
+ * Saxion Hogeschool Project
+ * Fighting_game
+ * File description
+ * ButtonSprite.cpp
+*/
+
+#include "ButtonSprite.hpp"
+
+fg::ButtonSprite::ButtonSprite()
+    : _hover(false), 
+    _click(false), 
+    _hovering(false),
+    _holding(false),
+    _holdingThis(false),
+    _clickLambda(nullptr),
+    _hoverLambda(nullptr)
+{}
+
+void fg::ButtonSprite::initialize(std::string filepath)
+{
+    _buttonTexture.loadFromFile(filepath);
+    _buttonSprite.setTexture(_buttonTexture);
+}
+
+void fg::ButtonSprite::setClick(bool click, std::string filepath, std::function<void()> lambda)
+{
+    if (click) {
+        _buttonTextureClick.loadFromFile(filepath);
+        _click = true;
+        _clickLambda = lambda;
+    }
+}
+
+void fg::ButtonSprite::setHover(bool hover, std::string filepath, std::function<void()> lambda)
+{
+    if (hover) {
+        _buttonTextureHover.loadFromFile(filepath);
+        _hover = true;
+        _hoverLambda = lambda;
+    }
+}
+
+void fg::ButtonSprite::setText(bool text, std::string filepath, std::string textString, int size, sf::Color color, float x, float y)
+{
+    if (text) {
+        _text.initialize(_buttonSprite, filepath, textString, size, color, x, y);
+    }
+}
+
+void fg::ButtonSprite::setSoundClick(bool sound, std::string filepath)
+{
+    if (sound) {
+        _soundClick.initialize(filepath);
+        _clickSound = true;
+    }
+}
+
+void fg::ButtonSprite::setSoundHover(bool sound, std::string filepath)
+{
+    if (sound) {
+        _soundHover.initialize(filepath);
+        _hoverSound = true;
+    }
+}
+
+void fg::ButtonSprite::click(sf::Event &event)
+{
+    if (!_click)
+        return;
+    if (event.type == sf::Event::MouseButtonPressed) {
+        _holding = true;
+        if (event.mouseButton.button == sf::Mouse::Left) {
+            if (_buttonSprite.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                _buttonSprite.setTexture(_buttonTextureClick);
+                _holdingThis = true;
+            }
+        }
+    }
+    if (event.type == sf::Event::MouseButtonReleased) {
+        if (event.mouseButton.button == sf::Mouse::Left) {
+            if (_buttonSprite.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                if (_clickSound)
+                    _soundClick.play();
+                if (_clickLambda != nullptr && _holdingThis) {
+                    _clickLambda();
+                }
+            }
+            _buttonSprite.setTexture(_buttonTexture);
+        }
+        _holding = false;
+        _holdingThis = false;
+    }
+}
+
+void fg::ButtonSprite::hover(sf::Event &event)
+{
+    if (!_hover)
+        return;
+    if (event.type == sf::Event::MouseMoved) {
+        if (_holding)
+            return;
+        if (_buttonSprite.getGlobalBounds().contains(event.mouseMove.x, event.mouseMove.y)) {
+            _buttonSprite.setTexture(_buttonTextureHover);
+            if (_hoverLambda != nullptr) {
+                _clickLambda();
+            }
+            if (!_hovering) {
+                _hovering = true;
+                if (_hoverSound)
+                    _soundHover.play();
+            }
+        } else {
+            _hovering = false;
+            if (!_holdingThis)
+                _buttonSprite.setTexture(_buttonTexture);
+        }
+    }
+}
+
+void fg::ButtonSprite::setPosition(float x, float y)
+{
+    _buttonSprite.setPosition(x, y);
+}
+
+void fg::ButtonSprite::scale(float x, float y)
+{
+    _buttonSprite.scale(x, y);
+}
+
+void fg::ButtonSprite::setVolume(float volume)
+{
+    _soundClick.setVolume(volume);
+    _soundHover.setVolume(volume);
+}
+
+sf::Texture &fg::ButtonSprite::getTexture()
+{
+    return _buttonTexture;
+}
+
+sf::Sprite &fg::ButtonSprite::getSprite()
+{
+    return _buttonSprite;
+}
+
+sf::Text &fg::ButtonSprite::getText()
+{
+    return _text.getText();
+}
+
+sf::Sound &fg::ButtonSprite::getSoundClick()
+{
+    return _soundClick.getSound();
+}
+
+sf::Sound &fg::ButtonSprite::getSoundHover()
+{
+    return _soundHover.getSound();
+}
---
+++ src/Components/ButtonSprite/ButtonSprite.hpp
@@ -0,0 +1,63 @@
+/**
+ * Saxion Hogeschool Project
+ * Fighting_game
+ * File description
+ * ButtonSprite.hpp
+*/
+
+#pragma once
+
+#include <iostream>
+#include <functional>
+#include <SFML/Graphics.hpp>
+#include "Text.hpp"
+#include "Sound.hpp"
+
+namespace fg
+{
+    class ButtonSprite 
+    {
+        public:
+            ButtonSprite();
+            ~ButtonSprite() = default;
+
+            void initialize(std::string filepath);
+            void setClick(bool click, std::string filepath = "", std::function<void()> lambda = nullptr);
+            void setHover(bool hover, std::string filepath = "", std::function<void()> lambda = nullptr);
+            void setText(bool text, std::string filepath = "", std::string textString = "", int size = 0, sf::Color color = sf::Color::White, float x = 0, float y = 0);
+            void setSoundClick(bool sound, std::string filepath = "");
+            void setSoundHover(bool sound, std::string filepath = "");
+
+            void click(sf::Event &event);
+            void hover(sf::Event &event);
+
+            void setPosition(float x, float y);
+            void scale(float x, float y);
+            void setVolume(float volume);
+
+            sf::Texture &getTexture();
+            sf::Sprite &getSprite();
+            sf::Text &getText();
+            sf::Sound &getSoundClick();
+            sf::Sound &getSoundHover();
+
+        private:
+            sf::Texture _buttonTexture;
+            sf::Texture _buttonTextureHover;
+            sf::Texture _buttonTextureClick;
+            sf::Sprite _buttonSprite;
+            bool _hover;
+            bool _click;
+            bool _clickSound;
+            bool _hoverSound;
+            bool _hovering;
+            bool _holding;
+            bool _holdingThis;
+            std::function<void()> _clickLambda;
+            std::function<void()> _hoverLambda;
+            fg::Text _text;
+            fg::Sound _soundClick;
+            fg::Sound _soundHover;
+            
+    };
+};
