ca20fa11db9ed4eec2dc5d8cc1294b32e0ad01e3
Nicolas <chastinlive.fr@gmail.com>
2023-10-04 17:17:37 +0200

    [Add] FileWriter Component FileWriter is used to save data in file and load data from file

---
+++ src/Components/FileWriter/FileWriter.cpp
@@ -0,0 +1,104 @@
+/**
+ * Saxion Hogeschool Project
+ * Fighting_game
+ * File description
+ * FileWriter.cpp
+*/
+
+#include "FileWriter.hpp"
+
+fg::FileWriter::FileWriter(std::string filepath)
+{
+    _filepath = filepath;
+}
+
+void fg::FileWriter::setFilepath(std::string filepath)
+{
+    _filepath = filepath;
+}
+
+void fg::FileWriter::printData()
+{
+    for (const auto &data : _data)
+    {
+        std::cout << data.first << ":" << data.second << std::endl;
+    }
+}
+
+void fg::FileWriter::save()
+{
+    std::ofstream file(_filepath);
+    if (!file.is_open())
+    {
+        std::cout << "Error opening file: " << _filepath << std::endl;
+        return;
+    }
+    for (const auto &data : _data)
+    {
+        std::cout << data.first << ":" << data.second << std::endl;
+        file << data.first << ":" << data.second << std::endl;
+    }
+    file.close();
+}
+
+void fg::FileWriter::save(std::string filepath)
+{
+    _filepath = filepath;
+    save();
+}
+
+void fg::FileWriter::addData(std::string dataName, std::string data)
+{
+    _data[dataName] = data;
+}
+
+void fg::FileWriter::addData(std::string dataName, const char *data)
+{
+    _data[dataName] = data;
+}
+
+void fg::FileWriter::addData(std::string dataName, char data)
+{
+    _data[dataName] = std::to_string(data);
+}
+
+void fg::FileWriter::addData(std::string dataName, int data)
+{
+    _data[dataName] = std::to_string(data);
+}
+
+void fg::FileWriter::addData(std::string dataName, float data)
+{
+    _data[dataName] = std::to_string(data);
+}
+
+void fg::FileWriter::addData(std::string dataName, bool data)
+{
+    _data[dataName] = std::to_string(data);
+}   
+
+bool fg::FileWriter::load()
+{
+    std::ifstream file(_filepath);
+    if (!file.is_open())
+    {
+        std::cout << "Error opening file: " << _filepath << std::endl;
+        return false;
+    }
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::string dataName = line.substr(0, line.find(":"));
+        std::string data = line.substr(line.find(":")+1, line.size());
+        _data[dataName] = data;
+    }
+    file.close();
+    printData();
+    return true;
+}
+
+bool fg::FileWriter::load(std::string filepath)
+{
+    _filepath = filepath;
+    return load();
+}
---
+++ src/Components/FileWriter/FileWriter.hpp
@@ -0,0 +1,58 @@
+/**
+ * Saxion Hogeschool Project
+ * Fighting_game
+ * File description
+ * FileWriter.hpp
+*/
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+
+namespace fg
+{
+    class FileWriter
+    {
+        public:
+            FileWriter() = default;
+            FileWriter(std::string filepath);
+            ~FileWriter() = default;
+
+            void printData();
+
+            void setFilepath(std::string filepath);
+
+            void save();
+            void save(std::string filepath);
+
+            void addData(std::string dataName, std::string data);
+            void addData(std::string dataName, const char *data);
+            void addData(std::string dataName, char data);
+            void addData(std::string dataName, int data);
+            void addData(std::string dataName, float data);
+            void addData(std::string dataName, bool data);
+
+            bool load();
+            bool load(std::string filepath);
+
+            template <typename T>
+            T getData(std::string dataName)
+            {
+                std::istringstream ss(_data[dataName]);
+                T data;
+
+                if (!(ss >> data)) {
+                    throw std::invalid_argument("Invalid input: " + _data[dataName]);
+                }
+                return data;
+            }
+
+        private:
+            std::unordered_map<std::string, std::string> _data;
+            std::string _filepath;
+    };
+}
