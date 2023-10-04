d97aa168bc59785745686f4a66347a928056969b
Nicolas <chastinlive.fr@gmail.com>
2023-10-04 15:18:26 +0200

    [add] RectSprite Component RectSprite is used to have a sprite with different frame that is not animated in real time

---
+++ src/Components/RectSprite/RectSprite.cpp
@@ -0,0 +1,69 @@
+/**
+ * Saxion Hogeschool Project
+ * Fighting_game
+ * File description
+ * RectSprite.cpp
+*/
+
+#include "RectSprite.hpp"
+
+fg::RectSprite::RectSprite()
+    : _sizeX(0),
+    _sizeY(0),
+    _numberOfFrames(0),
+    _currentFrame(0),
+    _isVertical(false)
+{
+}
+
+void fg::RectSprite::initialize(std::string filepath, float sizeX, float sizeY, int frames, int currentFrame, bool isVertical)
+{
+    _sizeX = sizeX;
+    _sizeY = sizeY;
+    _numberOfFrames = frames;
+    _currentFrame = currentFrame;
+    _isVertical = isVertical;
+    _rectTexture.loadFromFile(filepath);
+    _rectSprite.setTexture(_rectTexture);
+
+    if (_isVertical)
+        _rectSprite.setTextureRect(sf::IntRect(0, _currentFrame * _sizeY, _sizeX, _sizeY));
+    else
+        _rectSprite.setTextureRect(sf::IntRect(_currentFrame * _sizeX, 0, _sizeX, _sizeY));
+}
+
+void fg::RectSprite::changeFrame(int frame)
+{
+    if (frame < 0 || frame >= _numberOfFrames)
+        return;
+    _currentFrame = frame;
+    if (_isVertical)
+        _rectSprite.setTextureRect(sf::IntRect(0, _currentFrame * _sizeY, _sizeX, _sizeY));
+    else
+        _rectSprite.setTextureRect(sf::IntRect(_currentFrame * _sizeX, 0, _sizeX, _sizeY));
+}
+
+void fg::RectSprite::setPosition(float x, float y)
+{
+    _rectSprite.setPosition(x, y);
+}
+
+void fg::RectSprite::scale(float x, float y)
+{
+    _rectSprite.scale(x, y);
+}
+
+sf::Texture &fg::RectSprite::getTexture()
+{
+    return _rectTexture;
+}
+
+sf::Sprite &fg::RectSprite::getSprite()
+{
+    return _rectSprite;
+}
+
+int fg::RectSprite::getCurrentFrame() const
+{
+    return _currentFrame;
+}
---
+++ src/Components/RectSprite/RectSprite.hpp
@@ -0,0 +1,41 @@
+/**
+ * Saxion Hogeschool Project
+ * Fighting_game
+ * File description
+ * RectSprite.hpp
+*/
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+namespace fg
+{
+    class RectSprite
+    {
+        public:
+            RectSprite();
+            ~RectSprite() = default;
+
+            void initialize(std::string filepath, float sizeX, float sizeY, int frames, int currentFrame, bool isVertical = false);
+            void changeFrame(int frame);
+
+            void setPosition(float x, float y);
+            void scale(float x, float y);
+
+            sf::Texture &getTexture();
+            sf::Sprite &getSprite();
+            int getCurrentFrame() const;
+
+        private:
+            sf::Texture _rectTexture;
+            sf::Sprite _rectSprite;
+            int _sizeX;
+            int _sizeY;
+            int _numberOfFrames;
+            int _currentFrame;
+            bool _isVertical;
+    };
+};
